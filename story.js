0f762985fe2c34aab0b5263349690a7b4d8dda15
Yousef-laban <yousef.labn@fgmail.com>
2021-05-17 15:34:46 +0300

    added story.js

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = "yousef";
+let age = 26;
+let hoppy = "Hoarding Books";
+
+console.log (`Mr.${name} in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hoppy} practice!`);
